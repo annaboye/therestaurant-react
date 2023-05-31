6fe7735a4020141141a71b9a826240ee5cdcbe0a
Ksenia <ksenia.ivanovva@gmail.com>
2023-05-31 14:32:26 +0200

    feat/created controller

--- fed22s-backend/src/controllers/bookingController.js
+++ fed22s-backend/src/controllers/bookingController.js
@@ -5,13 +5,42 @@ exports.getAllBookings = async (req, res, next) => {
     const bookings = await Booking.find();
 
     if (!danceClasses) {
-      throw new NotFoundError("Finns inga klasser tyvärr!");
+      throw new NotFoundError("Finns inga bokningar tyvärr!");
     }
 
     return res.json({
-      data: danceClasses,
+      data: bookings,
     });
   } catch (error) {
     next(error);
   }
 };
+
+
+
+exports.createBooking = async (req, res, next) => {
+  const date = req.body.date;
+  const time = req.body.time;
+    const amountOfPersons = req.body.amountOfPersons;
+    const description = req.body.description || " "; 
+    const guest = req.body.guest; 
+
+
+
+  const newBooking = await Booking.create({
+    date: date,
+    time: time,
+      amountOfPersons: amountOfPersons,
+      description: description,
+    guest: guest
+  });
+
+  return res
+    .setHeader(
+      "Location",
+      `http://localhost:${process.env.PORT}/api/v1/bookings/${newBooking._id}`
+    )
+    .status(201)
+    .json(newBooking);
+
+}
