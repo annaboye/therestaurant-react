a51fe1de677228ae7d5951aed8724b838fc736c4
Ksenia <ksenia.ivanovva@gmail.com>
2023-06-01 13:34:00 +0200

    feat: added BookingView

--- fed22s-therestaurant/src/pages/Admin.tsx
+++ fed22s-therestaurant/src/pages/Admin.tsx
@@ -1,14 +1,15 @@
 import { getBookings } from "../services/getBookings";
 import { BookingView } from "../components/BookingView";
 
+import { deleteBookings } from "../services/deleteBooking";
 export const Admin = () => {
   return (
     <>
       <button onClick={getBookings}>Admin</button>
       <div>
-        {bookings.map((b) => (
+        {/*  {bookings.map((b) => (
           <BookingView booking={b} key={b.id}></BookingView>
-        ))}
+        ))} */}
       </div>
     </>
   );
