45616e86b7ee72b292afd1a3d765fd69121eb9c9
Ksenia <ksenia.ivanovva@gmail.com>
2023-06-06 16:03:25 +0200

    feat: fixed bug in IBooking interface

---
+++ fed22s-therestaurant/src/components/BookingList.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { IBooking } from "../models/IBooking";
+import { getBookings } from "../services/getBookings";
+
+export const BookingList = () => {
+  const [bookings, setBookings] = useState<IBooking[]>([]);
+
+  useEffect(() => {
+    const fetchBookings = async () => {
+      try {
+        const bookingsData = await getBookings();
+        setBookings(bookingsData);
+      } catch (error) {
+        console.error("Error fetching bookings:", error);
+      }
+    };
+
+    fetchBookings();
+  }, []);
+
+  return (
+    <div>
+      <h2>Bokningar</h2>
+      <ul>
+        {bookings.map((booking) => (
+          <li key={booking._id}>
+            <p>ID: {booking._id}</p>
+            <p>Datum: {booking.date}</p>
+            <p>Tid: {booking.time}</p>
+            <p>Antal personer: {booking.amountOfPersons}</p>
+            <p>Namn: {booking.guest.name}</p>
+            <p>Mail: {booking.guest.email}</p>
+            <p>Telefonnummer: {booking.guest.mobile}</p>
+            <p>Meddelande: {booking.description}</p>
+            <button>Ta bort bokning</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
--- fed22s-therestaurant/src/pages/Admin.tsx
+++ fed22s-therestaurant/src/pages/Admin.tsx
@@ -1,18 +1,51 @@
 import { getBookings } from "../services/getBookings";
 import { BookingView } from "../components/BookingView";
-
 import { createBooking } from "../services/createBooking";
+import { BookingList } from "../components/BookingList";
+import { useState } from "react";
+import { BookingForm } from "../components/BookingForm";
+
 export const Admin = () => {
-  const getBookings = () => {};
+  const [showForm, setShowForm] = useState(false);
+  const [showFirstChoice, setShowFirstChoice] = useState(true);
+  const [showSecondChoice, setShowSecondChoice] = useState(false);
+  const [showBookingList, setShowBookingList] = useState(false);
+
+  const handleShowForm = () => {
+    setShowForm(true);
+  };
+
+  const handleShowSecondChoice = () => {
+    setShowSecondChoice(true);
+    setShowFirstChoice(false);
+    setShowForm(false);
+  };
+
+  const handleShowBookingList = () => {
+    setShowBookingList(true);
+  };
   return (
     <>
-      <button onClick={getBookings}>Admin</button>
-      {/* <button onClick={createBooking}>Create</button> */}
-
       <div>
-        {/*  {bookings.map((b) => (
-          <BookingView booking={b} key={b.id}></BookingView>
-        ))} */}
+        {showFirstChoice && (
+          <>
+            <button onClick={handleShowSecondChoice}>Hantera bokningar</button>
+            <button onClick={handleShowForm}>Ny bokning</button>
+          </>
+        )}
+
+        {showSecondChoice && (
+          <>
+            <button onClick={handleShowBookingList}>
+              Hämta alla bokningar
+            </button>
+            <button>Välj datum</button>
+          </>
+        )}
+
+        {showForm && <BookingForm></BookingForm>}
+
+        {showBookingList && <BookingList></BookingList>}
       </div>
     </>
   );
