2b55f02de8eaf4c7239815f35c8035b40cc05f98
Ksenia <ksenia.ivanovva@gmail.com>
2023-06-06 09:44:03 +0200

    feat: changed fetchBookings function

--- fed22s-therestaurant/src/pages/BookingView.tsx
+++ fed22s-therestaurant/src/pages/BookingView.tsx
@@ -5,38 +5,36 @@ import { BookingContext } from "../contexts/BookingContext";
 import { getBookingById } from "../services/getBookingById";
 import { useParams } from "react-router";
 
-interface BookingWithId extends IBooking {
-  id: string;
-}
-
-export const BookingView = () => {
-  const { id } = useParams();
-  const { bookings } = useContext(BookingContext);
-  const [booking, setBooking] = useState<BookingWithId | null>(null);
+export const BookingView = ({ bookingId }: { bookingId: string }) => {
+  const [booking, setBooking] = useState<any>(null);
 
   useEffect(() => {
-    const foundBooking = bookings.find(
-      (booking) => booking.id === id
-    ) as BookingWithId;
-    setBooking(foundBooking || null);
-  }, [bookings, id]);
+    const fetchBooking = async () => {
+      try {
+        const fetchedBooking = await getBookingById(bookingId);
+        setBooking(fetchedBooking);
+      } catch (error) {
+        console.error("Error fetching booking:", error);
+      }
+    };
+
+    fetchBooking();
+  }, [bookingId]);
+
+  if (!booking) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div>
-      <h1>Booking View</h1>
-      {booking ? (
-        <div>
-          <p>ID: {booking.id}</p>
-          <p>Date: {booking.date}</p>
-          <p>Time: {booking.time}</p>
-          <p>Amount of Persons: {booking.amountOfPersons}</p>
-          <p>Guest Name: {booking.guest.name}</p>
-          <p>Guest Email: {booking.guest.email}</p>
-          <p>Guest Mobile: {booking.guest.mobile}</p>
-        </div>
-      ) : (
-        <p>Loading booking...</p>
-      )}
+      <h1>Booking Details</h1>
+      <p>ID: {booking.id}</p>
+      <p>Date: {booking.date}</p>
+      <p>Time: {booking.time}</p>
+      <p>Amount of Persons: {booking.amountOfPersons}</p>
+      <p>Guest Name: {booking.guest.name}</p>
+      <p>Guest Email: {booking.guest.email}</p>
+      <p>Guest Mobile: {booking.guest.mobile}</p>
     </div>
   );
 };
