2813b25c64d76ecc450a85f37507ea56fa387d68
Adam Stuborn <a.stuborn@gmail.com>
2023-06-08 16:18:10 +0200

    (feat) add filter function

--- fed22s-therestaurant/src/components/BookingList.tsx
+++ fed22s-therestaurant/src/components/BookingList.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { ChangeEvent, FormEvent, useContext, useEffect, useState } from "react";
 import { IBooking } from "../models/IBooking";
 import { getBookings } from "../services/getBookings";
 import { BookingView } from "./BookingView";
@@ -7,10 +7,34 @@ import { BookingContext } from "../contexts/BookingContext";
 export const BookingList = () => {
   const bookings = useContext(BookingContext);
 
+  const [userInputDate, setUserInputDate] = useState("");
+
+  const handleDates = (e: ChangeEvent<HTMLInputElement>) => {
+    setUserInputDate(e.target.value);
+  };
+
+  const handleSubmitDates = (e: FormEvent) => {
+    e.preventDefault();
+  };
 
   return (
     <div>
       <h2>Bokningar</h2>
+      <div className="form-group">
+        <form onSubmit={handleSubmitDates}></form>
+        <label htmlFor="date"> Välj datum:</label>
+        <input
+          type="date"
+          value={userInputDate}
+          onChange={handleDates}
+          name="date"
+          required
+        />
+      </div>
+      <input type="radio" name="date" />
+      18:00
+      <input type="radio" name="date" />
+      21:00
       <ul>
         {bookings.map((booking) => (
           <li key={booking._id}>
