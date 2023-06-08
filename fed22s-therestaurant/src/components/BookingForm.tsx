7137aeb7589dd5d5b8066ba40dfa4e6631635eca
Ksenia <ksenia.ivanovva@gmail.com>
2023-06-08 23:54:46 +0200

    fix: fixed text in BookingForm

--- fed22s-therestaurant/src/components/BookingForm.tsx
+++ fed22s-therestaurant/src/components/BookingForm.tsx
@@ -9,8 +9,6 @@ import { faCircleCheck } from "@fortawesome/free-solid-svg-icons";
 import { calculateTables } from "../functions/calculateTables";
 import { IBooking } from "../models/IBooking";
 
-
-
 const defaultForm = {
   date: "",
   time: "",
@@ -20,41 +18,45 @@ const defaultForm = {
 };
 
 interface BookingState {
-currentpage: Number
-error: boolean
-avalibleTables: boolean
-
+  currentpage: Number;
+  error: boolean;
+  avalibleTables: boolean;
 }
 
-interface IBookingFormProps{
-  changeShow: ()=>void
+interface IBookingFormProps {
+  changeShow: () => void;
 }
 
-export const BookingForm = ({changeShow}: IBookingFormProps) => {
-const [userInput, setUserInput] = useState(defaultForm);
-const [bookingState, setBookingState] =useState<BookingState>({currentpage: 1, error:false, avalibleTables: true})
-const [show18Avalible, setShow18Avalible] = useState(false);
-const [show21Avalible, setShow21Avalible] = useState(false);
-const [loading, setLoading] = useState(false);
-const [bookingId, setBookingId] = useState("");
-
+export const BookingForm = ({ changeShow }: IBookingFormProps) => {
+  const [userInput, setUserInput] = useState(defaultForm);
+  const [bookingState, setBookingState] = useState<BookingState>({
+    currentpage: 1,
+    error: false,
+    avalibleTables: true,
+  });
+  const [show18Avalible, setShow18Avalible] = useState(false);
+  const [show21Avalible, setShow21Avalible] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [bookingId, setBookingId] = useState("");
 
   const showSpinner = () => {
     setLoading(true);
-    setBookingState({...bookingState, currentpage: 0, avalibleTables: true})
+    setBookingState({ ...bookingState, currentpage: 0, avalibleTables: true });
     setTimeout(() => {
       setLoading(false);
-      setBookingState({...bookingState, currentpage: 4, avalibleTables: true})
-      ;
+      setBookingState({
+        ...bookingState,
+        currentpage: 4,
+        avalibleTables: true,
+      });
     }, 3000);
   };
 
   const handleSubmitBooking = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    
+
     showSpinner();
     confirm("gdpr.....");
-
 
     try {
       const newBooking = await createBooking(userInput);
@@ -63,59 +65,62 @@ const [bookingId, setBookingId] = useState("");
     } catch (error) {
       console.error(error);
     }
-
   };
 
-const searchAvalibleTables = async(e: FormEvent<HTMLFormElement>) => {
-e.preventDefault();
-changeShow();
-let thisDateBookings:IBooking[]= []
-try {
-thisDateBookings = await getBookingsByDate(userInput.date)
+  const searchAvalibleTables = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    changeShow();
+    let thisDateBookings: IBooking[] = [];
+    try {
+      thisDateBookings = await getBookingsByDate(userInput.date);
 
-      let bookings18 = thisDateBookings.filter((booking)=>booking.time==="18:00")
-      let bookings21 = thisDateBookings.filter((booking)=>booking.time==="21:00")
-      let tablesBooked18 = calculateTables(bookings18)
-      let tablesBooked21 = calculateTables(bookings21)
+      let bookings18 = thisDateBookings.filter(
+        (booking) => booking.time === "18:00"
+      );
+      let bookings21 = thisDateBookings.filter(
+        (booking) => booking.time === "21:00"
+      );
+      let tablesBooked18 = calculateTables(bookings18);
+      let tablesBooked21 = calculateTables(bookings21);
       let tablesLeft = false;
 
-      if(tablesBooked18<15 && +userInput.amountOfPersons <=6){
-        setShow18Avalible(true)
-       tablesLeft= true;
+      if (tablesBooked18 < 15 && +userInput.amountOfPersons <= 6) {
+        setShow18Avalible(true);
+        tablesLeft = true;
       }
-      if(tablesBooked21<15 && +userInput.amountOfPersons <=6){
-        setShow21Avalible(true)
-        tablesLeft= true;
+      if (tablesBooked21 < 15 && +userInput.amountOfPersons <= 6) {
+        setShow21Avalible(true);
+        tablesLeft = true;
       }
-      if(tablesBooked18<14 && +userInput.amountOfPersons >6){
-        setShow18Avalible(true)
-        tablesLeft= true;
+      if (tablesBooked18 < 14 && +userInput.amountOfPersons > 6) {
+        setShow18Avalible(true);
+        tablesLeft = true;
       }
-      if(tablesBooked21<14 && +userInput.amountOfPersons >6){
-        setShow21Avalible(true)
-        tablesLeft= true;
+      if (tablesBooked21 < 14 && +userInput.amountOfPersons > 6) {
+        setShow21Avalible(true);
+        tablesLeft = true;
       }
 
-      if(tablesLeft){
-     setBookingState({...bookingState, currentpage: 2, avalibleTables: true})
+      if (tablesLeft) {
+        setBookingState({
+          ...bookingState,
+          currentpage: 2,
+          avalibleTables: true,
+        });
+      } else {
+        setBookingState({ ...bookingState, avalibleTables: false });
       }
-     else{
-      setBookingState({...bookingState, avalibleTables: false})
-     }
-
     } catch (error) {
-      setBookingState({...bookingState, error: true})
+      setBookingState({ ...bookingState, error: true });
     }
 
+    console.log(thisDateBookings);
+  };
 
-    console.log(thisDateBookings)
-
-};
-
-const chooseTime = (e:FormEvent) => {
-e.preventDefault();
-setBookingState({...bookingState, currentpage: 3})
-};
+  const chooseTime = (e: FormEvent) => {
+    e.preventDefault();
+    setBookingState({ ...bookingState, currentpage: 3 });
+  };
 
   const handleChangeOne = (e: ChangeEvent<HTMLInputElement>) => {
     const name = e.target.name;
@@ -132,7 +137,7 @@ setBookingState({...bookingState, currentpage: 3})
 
   return (
     <div className="form-wrapper ">
-      {bookingState.currentpage===1 && (
+      {bookingState.currentpage === 1 && (
         <form
           onSubmit={(e) => {
             searchAvalibleTables(e);
@@ -143,7 +148,7 @@ setBookingState({...bookingState, currentpage: 3})
             <input
               type="date"
               value={userInput.date}
-              min= {new Date().toISOString().split('T')[0]}
+              min={new Date().toISOString().split("T")[0]}
               onChange={handleChangeOne}
               name="date"
               required
@@ -165,12 +170,18 @@ setBookingState({...bookingState, currentpage: 3})
           <button>sök lediga bord</button>
         </form>
       )}
-      
-{bookingState.error && <div> tyvärr går det inte att göra bokningar just nu </div>}
 
- {!bookingState.avalibleTables && <div>tyvärr finns det inga lediga bord denna dag, gör en ny sökning</div>}
+      {bookingState.error && (
+        <div> tyvärr går det inte att göra bokningar just nu </div>
+      )}
 
-      {bookingState.currentpage===2 && (
+      {!bookingState.avalibleTables && (
+        <div>
+          tyvärr finns det inga lediga bord denna dag, gör en ny sökning
+        </div>
+      )}
+
+      {bookingState.currentpage === 2 && (
         <form onSubmit={chooseTime}>
           <div className="form-group">
             <label>
@@ -183,8 +194,8 @@ setBookingState({...bookingState, currentpage: 3})
                 required
               >
                 <option value=""></option>
-              {show18Avalible &&<option value="18:00">18:00</option>}
-              {show21Avalible &&<option value="21:00">21:00</option>}
+                {show18Avalible && <option value="18:00">18:00</option>}
+                {show21Avalible && <option value="21:00">21:00</option>}
               </select>
             </label>
           </div>
@@ -192,7 +203,7 @@ setBookingState({...bookingState, currentpage: 3})
         </form>
       )}
 
-      {bookingState.currentpage===3 && (
+      {bookingState.currentpage === 3 && (
         <form onSubmit={handleSubmitBooking}>
           <div className="form-group">
             <label htmlFor="name"> Namn:</label>
@@ -244,10 +255,10 @@ setBookingState({...bookingState, currentpage: 3})
         </div>
       )}
 
-      {bookingState.currentpage===4 && (
+      {bookingState.currentpage === 4 && (
         <div className="booking-id-wrapper">
           <h3>Tack för din bokning!</h3>
-          <h4> Här ärditt bokningsnummer:</h4>
+          <h4> Här är ditt bokningsnummer:</h4>
           <p>{bookingId}</p>
 
           <div>
@@ -258,7 +269,3 @@ setBookingState({...bookingState, currentpage: 3})
     </div>
   );
 };
-
-
-
-
